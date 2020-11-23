1771183614d4bbfe203454b64af7de0825b7d983
Amit Singh <cm8204@hotmail.com>
2020-11-23 17:21:57 +0530

    clear all unnessary code

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 const students = require("./student");
 const cors = require("cors");
+
 app.use(cors());
 app.use(express.json());
+
 app.get("/users", (req, res) => {
   res.status(200).json(students);
 });
